79ae81797771809c0548f8f5c628241e73d49380
dma1991 <mandrupdaniel@gmail.com>
2018-01-18 21:18:56 +0100

    Changed sender to other broadcast.

--- skipi_sender.py
+++ skipi_sender.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-UDP_IP = "10.0.0.255"
+UDP_IP = '<broadcast>'
 UDP_PORT = 5005
 MSG_MAX_LEN = 20
 RAND_MIN = 1
@@ -14,12 +14,15 @@ print "UDP target port:", UDP_PORT
 led_mode = RAND_MIN
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+sock.bind(('', 0))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
 while True:
     #message = str(random.randint(RAND_MIN, RAND_MAX))
     message = str(led_mode)
     sock.sendto(message, (UDP_IP, UDP_PORT))
     print "Sent: ", message
-    sleep(LED_INTERVAL)
+    time.sleep(LED_INTERVAL)
     led_mode += 1
     if led_mode > RAND_MAX:
         led_mode = RAND_MIN
